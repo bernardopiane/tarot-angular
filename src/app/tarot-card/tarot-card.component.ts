7d566be3b3d39da76d2fe8ecaaeb4cb06b46b7c0
bernardopiane <bernardopiane@gmail.com>
2022-02-17 19:28:57 -0300

    Flip card logic

--- src/app/tarot-card/tarot-card.component.ts
+++ src/app/tarot-card/tarot-card.component.ts
@@ -1,4 +1,14 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CardDetailComponent } from '../card-detail/card-detail.component';
+import { TarotDeckComponent } from '../tarot-deck/tarot-deck.component';
 
 @Component({
   selector: 'app-tarot-card',
@@ -13,21 +23,46 @@ export class TarotCardComponent implements OnInit {
 
   @Input() imageBackCard: any;
 
+  @Input() hasChoosenCard: boolean = false;
+
+  @Output('setHasChoosenCard') setHasChoosenCard: EventEmitter<any> =
+    new EventEmitter();
+
   showBack: boolean = false;
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   public flipCard() {
-    // Select the class card-image
-    const cardImage = this.el.nativeElement.querySelector('.mat-card');
-    cardImage.classList.toggle('flip');
+    // If the card flipped, show the card details on a modal window
+    if (!this.showBack) {
+      this.dialog.open(CardDetailComponent, {
+        data: {
+          card: this.card,
+          imagesUrl: this.imagesUrl,
+        },
+      });
+    }
+    // Allows only one card to be selected
+    else if (this.hasChoosenCard) {
+      alert('You have already choosen a card');
+    }
+    // If the card is not flipped, flip it
+    else {
+      this.setHasChoosenCard.emit(true);
+
+      // Select the class card-image
+      const cardImage = this.el.nativeElement.querySelector('.mat-card');
+      cardImage.classList.toggle('flip');
+      this.showBack = !this.showBack;
+    }
   }
 
   // Set the element classlist to flip
   public showBackCard() {
     const cardImage = this.el.nativeElement.querySelector('.mat-card');
     cardImage.classList.add('flip');
+    this.showBack = true;
   }
 }
