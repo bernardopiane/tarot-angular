e71f91134052d2a961219fac97294c40ecc0e53f
bernardopiane <bernardopiane@gmail.com>
2022-02-17 19:27:48 -0300

    Card Details init

---
+++ src/app/card-detail/card-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-card-detail',
+  templateUrl: './card-detail.component.html',
+  styleUrls: ['./card-detail.component.scss'],
+})
+export class CardDetailComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  ngOnInit(): void {}
+}
